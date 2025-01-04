7f3abcb20db87d7c5e5bb9b160f5e2f81098c378
Nilay1999 <nilayparmar19@gmail.com>
2025-01-04 22:35:02 +0530

    feat: :sparkles: enhance authentication flow with user response structure and JWT token generation
    Signed-off-by: Nilay1999 <nilayparmar19@gmail.com>

--- backend/types/auth.go
+++ backend/types/auth.go
@@ -4,3 +4,13 @@ type AuthType struct {
 	Identifier string `json:"identifier" binding:"required" required:"username or email is required"`
 	Password   string `json:"password" binding:"required" required:"password is required"`
 }
+
+type UserResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+}
+
+type SignupResDto struct {
+	User  UserResponse `json:"user"`
+	Token string       `json:"token"`
+}
